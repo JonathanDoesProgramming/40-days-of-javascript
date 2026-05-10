ba51f1e45f5a8dc9ed171697f4bb08db36d71b77
JonathanDoesProgramming <jonathanbayhonpersonal01@gmail.com>
2026-05-10 20:18:21 +0800

    Created a multiplication table

--- Day 05/task-assignments/task-1.js
+++ Day 05/task-assignments/task-1.js
@@ -1,7 +1,29 @@
-for (let i = 0; i <= 5; i++) {
-    for (let j = 0; j <= 5; j++) {
-        if (i <= 5 && j <= 5) {
-            console.log("*");
-        }
+// Solution 1: 
+let rows = 5; // height of the pyramid
+
+for (let r = 1; r <= rows; r++) {
+    let line = "";
+    for (let c = 1; c <= r; c++) {
+        line += "* ";  // add a star and a space
     }
+    console.log(line);
+}
+
+// Solution 2: 
+let output = ""; 
+for (let i = 1; i <= 5; i++) { // this loop will create each rows or lines
+
+    for (let j = 1; j <= i; j++) { // this loop will print asterisk(*)
+        
+        // like below codes you dont need to check or compare i and j for loop is doing on its own
+        // if (i <= 5 && j <= 5) { 
+            // console.log("*");
+        // }
+        
+        output += "*"; // this is responsible to append a * at the end of output variable
+        
+    }
+    
+    console.log(output); // here you will find appended asterisk's output
+    output = ""; // must rest output variable. other wise won't get expected result. 
 }
---
+++ Day 05/task-assignments/task-2.js
@@ -0,0 +1,5 @@
+let n = 4; 
+
+for (let m = 1; m <= 10; m++) {
+    console.log(`${n} x ${m} = ${n * m}`); 
+}
